69100088f4490720acdfb8f7eeb1c6240652a702
muhammad ali <aliformer768@gmail.com>
2022-10-20 10:57:38 +0700

    add new routes and readme

---
+++ README.md
@@ -0,0 +1,2 @@
+## JSON API Link
+#### https://www.getpostman.com/collections/ee3bc7bf888bf3ab7ab9
--- start/routes.ts
+++ start/routes.ts
@@ -23,6 +23,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('/register', 'AuthController.register')
 Route.post('/login', 'AuthController.login')
+Route.get('/logout', 'AuthController.logout')
 
 
 
